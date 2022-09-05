8e8a6c562841be31e89be6ec027a144499f834aa
Saehan <jungsae@oregonstate.edu>
2022-09-06 07:17:48 +0900

    [ADD] user_login_API
    [Fix] 비즈니스로직 위치 수정

--- services/userService.js
+++ services/userService.js
@@ -1,15 +1,26 @@
 const jwt = require('jsonwebtoken');
+const axios = require('axios').default;
 const userDao = require('../models/userDao');
 
-const logInWithKakao = async (kakaoUser) => {
-    const user = await userDao.getUserByEmail(kakaoUser);
+const logInWithKakao = async (ACCESS_TOKEN) => {
+    const { data: kakaoUser } = await axios("https://kapi.kakao.com/v2/user/me",
+        {
+            headers: {
+            Authorization: `Bearer ${ACCESS_TOKEN}`
+            },
+        });
 
-    if(user){
-        const jwtToken = jwt.sign({ sub: user.kakao_id, userName: user.nickname, email: user.email}, process.env.JWT_SECRET, { expiresIn: '1h' });
-        return jwtToken;
+    const usercheck = await userDao.getUserByEmail(kakaoUser);
+
+    if(!usercheck){
+        await userDao.createUser(kakaoUser);
     };
 
-    await userDao.createUser(kakaoUser);
+    const user = await userDao.getUserByEmail(kakaoUser);
+
+    const jwtToken = await jwt.sign({ sub: user.kakao_id, userName: user.nickname, email: user.email}, process.env.JWT_SECRET, { expiresIn: '24h' });
+
+    return jwtToken;
 };
 
 module.exports = {
