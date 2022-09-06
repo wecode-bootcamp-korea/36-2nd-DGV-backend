4101977a454d24efe8d710674577a70c1c535cf6
MH <myungh5@MHui-MacBookAir.local>
2022-09-06 16:55:11 +0900

    [ADD] 영화 전체 제목 불러오기

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const movieRouter = require("./movieRouter");
+
+router.use("/movies", movieRouter.router);
+
 module.exports = router;
